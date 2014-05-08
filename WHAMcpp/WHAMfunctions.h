77a0c4e076402a40bbf6a486e9ba460475cf5216
David Walker <dwalker86@gmail.com>
2014-05-08 13:34:28 -0500

    Added the C++ WHAM code
    Note the majority of this code was written by Andrew Ritchie in the Webb
    group. I helped develop a lot of the functions, headers and learned C++
    alongside him. This is an adaptation of python code developed by Dr.
    Daniel Ensign while in the Webb group.
    
    This is a multidimensional Weighted-Histrogram Analysis program
    writting in C++.  For more information about the algorithm employed,
    refer to 'BEFEUS: Bayesian Estimation of Free Energy for Umbrella
    Sampling' by Dr. Daniel L. Ensign.
    Currently, this build can only handle up to 3 degrees of freedom,
    but has only been tested up to 2.
    NOTE 1: Be sure to make sure the file list format correctly adheres
    to what is listed under the --filelist option information.  The
    trajectory #
    needs to be unique for each individual simulation.  This parameter is
    used to group different degrees of freedom to the same biased
    trajectory.  The numbering can start at 0 -or- 1.
    NOTE 2:  When using the --first_frame and --last_frame flags, the
    selection will show up at the begining and the end of the run, but the
    number
    of frames in each trajectory will still be listed as the total number of
    
    frames for the trajectory, rather than however many were used.
    NOTE 3: When assigning bin widths and coordinate ranges, use multiple
    invocations of -w or include the values as a string within quotations
    (the
    order is the same order as the dimensions are listed in the --filelist
    file).
    NOTE 4: Make sure the unit (X) on --min/max_coordinate and --bin_width
    match the coordinate bias value, flat range, and force constant in the
    --filelist input.
    NOTE 4: Assuming every trajectory has the same number of frames.  I can
    fix this assumption, but for now it's easier not to.

---
+++ WHAMcpp/WHAMfunctions.h
@@ -0,0 +1,38 @@
+//
+//  WHAMfunctions.h
+//  mcWHAM
+//
+
+#ifndef mcWHAM_WHAMfunctions_h
+#define mcWHAM_WHAMfunctions_h
+
+#include "BefeusTools.h"
+
+#endif
+
+
+class DOWHAM
+{
+    int points, nstates, nexperiments;
+    int * ncounts, * sample_sizes;
+    long double * wvec;
+    long double ** trajectory;
+    long double ** omega_vectors;
+    long double ** omega_transpose;
+    long double * opt_trajectory;
+public:
+    int Points();
+    long double * OptTrajectory();
+    
+    void DoWham( long double * omega_vec, int * counts, int * samples, long double ** omegas, int states, int nsamples, int niter, float tol);
+    
+    long double * WhamStep( long double * trajectory );
+    
+    long double dot_product( double long * a, double long * b, int size );
+    
+    void OmegaTranspose();
+    
+    long double Sum( long double * an_array, int length_of_an_array );
+    
+    long double * square_diff( long double * array_1, long double * array_2, int length_of_arrays );
+};
---
+++ WHAMcpp/fileoptions.h
@@ -0,0 +1,40 @@
+//
+//  fileoptions.h
+//  mcWHAM
+//
+
+
+#ifndef mcWHAM_fileoptions_h
+#define mcWHAM_fileoptions_h
+
+#include <algorithm>
+#include "WHAMfunctions.h"
+
+#endif
+
+/* Standard help message */
+void userinfo(double T, double begin, double end, double bin_size, int dof, double tol, int niter, bool bVerbose);
+
+/* Option parsing */
+char* getCmdOption(char ** begin, char ** end, const std::string & option);
+
+struct range
+{
+    double * values;
+    int n_values;
+};
+
+range getMultiCmdOption(char ** begin, char ** end, const std::string & option);
+
+/* Checking for options to parse though */
+bool cmdOptionExists(char** begin, char** end, const std::string & option);
+   
+/* How many lines */
+int get_n_lines( char * input );
+
+void nD_WHAM_grouping( TorsionExperiment * experiment, int dof, int n_files, int first_experiment, int **&group_trajectories, bool bVerbose );
+
+
+void compare_len( int a, std::string alabel, int b, std::string blabel );
+
+
